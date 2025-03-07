bc9d6f66a0a8cd8e61c8f75d980bd20582c008f1
Amit-Pujari <poojariamit2003@gmail.com>
2025-03-08 01:33:44 +0530

    first commit

---
+++ frontend/RecipeSharing/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div className='footer'>
+        <footer>
+                <p>&copy; 2025 Recipedia. All rights reserved.</p>
+        </footer>
+    </div>
+  )
+}
